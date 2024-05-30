e0241528c6675c65d7741f62244dff60791a1e8d
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-05-30 08:45:15 -0400

    Con sqlachemy

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, Depends
 from pydantic import BaseModel
 from datetime import datetime, timezone, timedelta
-from typing import List, Union
+from typing import List, Union, Annotated
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
 from passlib.context import CryptContext
@@ -14,8 +14,10 @@ SECRET_KEY = "27A0D7C4CCCE76E6BE39225B7EEE8BD0EF890DE82D49E459F4C405C583080AB0"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
 class Evento(BaseModel): 
-    ID : int
+    ID : str
     titulo : str
     descripcion : str
     fechaHora : datetime
@@ -26,9 +28,74 @@ class Usuario(BaseModel):
     usuario : str
     nombre_real : str
     contraseña : str
+    color_favorito : str
+
+class UsuarioEnBD(BaseModel): 
+    contraseñaHasheada : str
+
+class Token(BaseModel): 
+    usuario : str
+    token_acceso : str
+    tipo_token : str
+
+class DataToken(BaseModel): 
+    usuario : Union[str, None] = None
 
 usuarios : list[Usuario] = []
 eventos : list[Evento] = []
+
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+# def dumb_decode_token(token):
+#     return Usuario(usuario=token + "dummydecoded", color_favorito="john@example.com", full_name="John Doe")
+
+def get_user(db, usuario: str):
+    if usuario in db:
+        user_dict = db[usuario]
+        return UsuarioEnBD(**user_dict)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    user = dumb_decode_token(token)
+    credentials_exception = HTTPException(
+            status_code= status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = DataToken(usuario=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(usuarios, usuario=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def autheticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    hashed_password = get_password_hash(user.hashed_password)
+    if not verify_password(password, hashed_password):
+        return False
+    return user
 
 @app.post('/registrar')
 async def registrar(usuario : Usuario): 
@@ -39,6 +106,7 @@ async def registrar(usuario : Usuario):
                 detail='Ya existe un usuario con el mismo nombre'
             )
     usuarios.append(usuario)
+    return usuario
 
 @app.post('/inciarsesion')
 async def iniciar_sesion(): 
@@ -54,22 +122,20 @@ async def listar_eventos():
 
 @app.post('/evento', status_code=status.HTTP_201_CREATED)
 async def crear_evento(evento : Evento): 
-    estos = filter(lambda x: x.ID == evento.ID, eventos)
-    if estos != None: 
-        raise HTTPException(
-            status_code=status.HTTP_406_NOT_ACCEPTABLE, 
-            detail='No se puede tener un evento con una id que ya exista'
-        )
-    else: 
-        eventos.append(evento)
-        return evento
+    for esto in eventos: 
+        if esto.ID == evento.ID: 
+            raise HTTPException(
+                status_code=status.HTTP_406_NOT_ACCEPTABLE, 
+                detail='No se puede tener un evento con una id que ya exista'
+            )
+    eventos.append(evento)
+    return evento
 
 @app.get('/evento/{id}')
-async def buscar_evento(id : int): 
+async def buscar_evento(id : str): 
     for esto in eventos: 
         if esto.ID == id: return esto
-    else: 
-        raise HTTPException(
+    raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND, 
         detail="No se pudo encontrar ese evento"
     )
@@ -95,32 +161,40 @@ async def listar_eventos_no_hechos():
     else: return no_hechos
 
 @app.put('/evento/{id}')
-async def modificar_evento(id : int, evento : Evento): 
+async def modificar_evento(id : str, evento : Evento): 
     if id != evento.ID: 
         raise HTTPException(
             status_code=status.HTTP_406_NOT_ACCEPTABLE, 
             detail='El id del parámetro y la del evento deben ser iguales'
         )
-    real = filter(lambda x: x.ID == id, eventos)
-    if real == None: 
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, 
-            detail='No se pudo encontrar un evento con tal id'
-        )
-    eventos[eventos.index(real)] = evento
-    return evento
+    for i in range(len(eventos)): 
+        if eventos[i].ID == id: 
+            eventos[i] = evento
+            return evento
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, 
+        detail='No se pudo encontrar un evento con tal id'
+    )
 
 @app.put('/evento_notas/{id}')
-async def añadir_notas(id, notas): 
-    return f'Un evento {id} con estas notas {notas}'
+async def añadir_notas(id : str, notas : str): 
+    for i in range(len(eventos)): 
+        if eventos[i].ID == id: 
+            eventos[i].notas.append(notas)
+            return eventos[i]
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, 
+        detail='No se pudo encontrar un evento con tal id'
+    )
 
 @app.delete('/evento/{id}')
-async def eliminar_evento(id : int): 
-    evento = filter(lambda x: x.ID == id, eventos)
-    if evento == None: 
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, 
-            detail='No se pudo encontrar un evento con tal id'
-        )
-    eventos.remove(evento)
-    return evento
+async def eliminar_evento(id : str): 
+    for i in range(len(eventos)): 
+        if eventos[i].ID == id: 
+            evento = eventos[i]
+            eventos.remove(evento)
+            return evento 
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, 
+        detail='No se pudo encontrar un evento con tal id'
+    )
